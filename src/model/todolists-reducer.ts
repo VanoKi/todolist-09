83fdede4ac2d9ce1818e32acce17f4253f05e6f7
VanoKi <kirsvano@gmail.com>
2025-05-21 19:45:59 +0300

    fix: createAction

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,5 +1,10 @@
 import {v1} from 'uuid'
 import type {FilterValues, Todolist} from '../App'
+import {createAction} from "@reduxjs/toolkit";
+
+export const deleteTodolistAC = createAction<{ id: string }>('todolists/deleteTodoList')
+export const changeTodolistTitleAC = createAction<{id: string, title: string}>('todolists/changeTodolistTitle')
+export const changeTodolistFilterAC = createAction<{id:string, filter: FilterValues}>('todolists/changeTodolistFilter')
 
 const initialState: Todolist[] = []
 
@@ -23,20 +28,8 @@ export const todolistsReducer = (state: Todolist[] = initialState, action: Actio
   }
 }
 
-export const deleteTodolistAC = (id: string) => {
-  return {type: 'delete_todolist', payload: { id }} as const
-}
-
 export const createTodolistAC = (title: string) => {
   return {type: 'create_todolist', payload: { title, id: v1() }} as const
-}
-
-export const changeTodolistTitleAC = (payload: {id: string, title: string}) => {
-  return {type: 'change_todolist_title', payload} as const
-}
-
-export const changeTodolistFilterAC = (payload: {id: string, filter: FilterValues}) => {
-  return {type: 'change_todolist_filter', payload} as const
 }
 
 export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
