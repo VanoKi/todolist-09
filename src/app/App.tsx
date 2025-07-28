f53f811b4e56b09167e97ffc941be861c78bc962
VanoKi <kirsvano@gmail.com>
2025-07-28 16:39:28 +0300

    feat: tasks and todoliss-selectors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -21,10 +21,9 @@ import Switch from '@mui/material/Switch'
 import CssBaseline from '@mui/material/CssBaseline'
 import {containerSx} from '../TodolistItem.styles'
 import {NavButton} from '../NavButton'
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "./store.ts";
 import {useAppSelector} from "../common/hooks/useAppSelector.ts";
 import {useAppDispatch} from "../common/hooks/useAppDispatch.ts";
+import {RootState} from "./store.ts";
 
 export type Todolist = {
   id: string
@@ -44,12 +43,12 @@ export type TasksState = Record<string, Task[]>
 
 type ThemeMode = 'dark' | 'light'
 
+
 export const App = () => {
-  const todolists = useAppSelector((state) => state.todolists)
-  const tasks = useAppSelector((state) => state.tasks)
+  const todolists = useAppSelector(selectTodolists)
+  const tasks = useAppSelector(selectTasks)
   const dispatch = useAppDispatch()
   const [themeMode, setThemeMode] = useState<ThemeMode>('light')
-
   const theme = createTheme({
     palette: {
       mode: themeMode,
